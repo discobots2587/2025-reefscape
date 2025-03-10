2da50b3a795a9ec7c86d74e91b6b4a06e2c39f3b
Ian Lu <104738510+ianylu@users.noreply.github.com>
2025-03-09 20:23:36 -0500

    adjusted command to wait for setpoint to move completely

--- src/main/java/frc/robot/commands/scoreL3test.java
+++ src/main/java/frc/robot/commands/scoreL3test.java
@@ -5,20 +5,40 @@ import com.pathplanner.lib.auto.NamedCommands;
 
 public class scoreL3test extends Command {
     private Command scoreCommand;
+    private boolean isScoreCommandFinished;
 
     public scoreL3test() {
         scoreCommand = NamedCommands.getCommand("scoreL3");
+        isScoreCommandFinished = false;
     }
 
     @Override
     public void initialize() {
-        if(scoreCommand != null && !scoreCommand.isScheduled()) {
+        if (scoreCommand != null && !scoreCommand.isScheduled()) {
             scoreCommand.schedule();
+            isScoreCommandFinished = false;
+        }
+    }
+
+    @Override
+    public void execute() {
+        // Check if the scoreCommand has finished
+        if (scoreCommand != null && scoreCommand.isFinished()) {
+            isScoreCommandFinished = true;
         }
     }
 
     @Override
     public boolean isFinished() {
-        return true; // Instant command
+        // The command is finished when the scoreCommand has completed
+        return isScoreCommandFinished;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        // If the command is interrupted, cancel the scoreCommand
+        if (scoreCommand != null) {
+            scoreCommand.cancel();
+        }
     }
 }
